05b398293580cb7b8be39df18aaedc6e9ccb64d0
Alex <farego@yandex.ru>
2022-12-02 09:16:21 +0300

    Connection errors handling added

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,4 +1,4 @@
-from django.core.paginator import Paginator
+import django
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import Book
@@ -12,27 +12,30 @@ def index(request):
 
 def books_view(request):
     template = 'books/books_list.html'
-
-    books = Book.objects.all().order_by('name')
-    context = {'books': books}
-    return render(request, template, context)
+    try:
+        books = Book.objects.all().order_by('name')
+        context = {'books': books}
+        return render(request, template, context)
+    except django.db.utils.OperationalError:
+        return HttpResponse('Ошибка подключения к базе данных.. :(')
 
 
 def pub_date_view(request, date):
     template = 'books/books_by_pub_date.html'
-
     date_obj = datetime.strptime(date, '%Y-%m-%d').date()
-    books = Book.objects.filter(pub_date=date_obj)
+    try:
+        books = Book.objects.filter(pub_date=date_obj)
 
-    pub_dates_dict_list = list(Book.objects.order_by('pub_date').distinct('pub_date').values('pub_date'))
-    pub_dates = [list(d.values())[0] for d in pub_dates_dict_list]
-    ind = pub_dates.index(date_obj)
+        pub_dates_dict_list = list(Book.objects.order_by('pub_date').distinct('pub_date').values('pub_date'))
+        pub_dates = [list(d.values())[0] for d in pub_dates_dict_list]
+        ind = pub_dates.index(date_obj)
 
-    context = {
-        'date': date,
-        'previous_page': pub_dates[ind-1].strftime('%Y-%m-%d') if ind > 0 else None,
-        'next_page': pub_dates[ind+1].strftime('%Y-%m-%d') if ind < len(pub_dates) - 1 else None,
-        'books': books
-    }
-
-    return render(request, template, context)
+        context = {
+            'date': date,
+            'previous_page': pub_dates[ind-1].strftime('%Y-%m-%d') if ind > 0 else None,
+            'next_page': pub_dates[ind+1].strftime('%Y-%m-%d') if ind < len(pub_dates) - 1 else None,
+            'books': books
+        }
+        return render(request, template, context)
+    except django.db.utils.OperationalError:
+        return HttpResponse('Ошибка подключения к базе данных.. :(')
