65fe88de50a9f401b0e4a3e6cb4900f200a4a4da
Alex <farego@yandex.ru>
2023-01-17 16:06:21 +0300

    All tests are done

--- 3.4-django-testing/django_testing/students/serializers.py
+++ 3.4-django-testing/django_testing/students/serializers.py
@@ -1,10 +1,44 @@
+from django.conf import settings
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
-from students.models import Course
+from students.models import Course, Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ["id", "name", "birth_date"]
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    students = StudentSerializer(many=True)
 
     class Meta:
         model = Course
         fields = ("id", "name", "students")
+
+    def create(self, validated_data):
+        students = validated_data.pop('students', None)
+        course = super().create(validated_data)
+        if students:
+            student_objs = [Student(**data) for data in students]
+            Student.objects.bulk_create(student_objs)
+            course.students.add(*student_objs)
+        return course
+
+    def update(self, instance, validated_data):
+        students = validated_data.pop('students', None)
+        course = super().update(instance, validated_data)
+        if students:
+            student_objs = [Student(**data) for data in students]
+            course.students.set(student_objs)
+        return course
+
+    @staticmethod
+    def validate_students(value):
+        students_qty = len(value)
+        if students_qty > settings.MAX_STUDENTS_PER_COURSE:
+            raise ValidationError(f'Q-ty {students_qty} of students per course must not be greater than '
+                                  f'{settings.MAX_STUDENTS_PER_COURSE}!')
+        return value
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,173 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+from students.models import Course, Student
+
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
+
+@pytest.mark.django_db
+def test_retrieve_course(api_client, course_factory):
+    # Arrange
+    courses = course_factory(_quantity=1)
+    course = courses[0]
+
+    # Action
+    url = reverse('courses-detail', args=[course.pk])
+    response = api_client.get(url)
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    resp_json = response.json()
+    assert resp_json['name'] == course.name
+
+
+@pytest.mark.django_db
+def test_list_course(api_client, course_factory):
+    # Arrange
+    courses = course_factory(_quantity=10)
+
+    # Action
+    url = reverse('courses-list')
+    response = api_client.get(url)
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    resp_json = response.json()
+    for ind, course in enumerate(courses):
+        assert resp_json[ind]['name'] == course.name
+
+
+@pytest.mark.django_db
+def test_filter_course_by_id(api_client, course_factory):
+    # Arrange
+    courses = course_factory(_quantity=10)
+    course = courses[3]
+
+    # Action
+    url = reverse('courses-list')
+    response = api_client.get(url, data={'id': course.pk})
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    resp_json = response.json()
+    assert resp_json[0]['id'] == course.pk
+
+
+@pytest.mark.django_db
+def test_filter_course_by_name(api_client, course_factory):
+    # Arrange
+    courses = course_factory(_quantity=10)
+    course_name = courses[3].name
+
+    # Action
+    url = reverse('courses-list')
+    response = api_client.get(url, data={'name': course_name})
+
+    # Assert
+    assert response.status_code == status.HTTP_200_OK
+    resp_json = response.json()
+    for course in resp_json:
+        assert course['name'] == course_name
+
+
+@pytest.mark.django_db
+def test_create_course_success(api_client, settings):
+    # Arrange
+    count = Course.objects.count()
+    settings.MAX_STUDENTS_PER_COURSE = 2
+
+    # Action
+    url = reverse('courses-list')
+    response = api_client.post(url, data={
+        'name': 'Python',
+        'students': [
+            {'name': 'Inna'},
+            {'name': 'Ivan'},
+        ]
+    }
+                               )
+
+    # Assert
+    assert response.status_code == status.HTTP_201_CREATED
+    assert Course.objects.count() == count + 1
+    resp_json = response.json()
+    assert resp_json['name'] == 'Python'
+
+
+@pytest.mark.django_db
+def test_create_course_failure(api_client, settings):
+    # Arrange
+    count = Course.objects.count()
+    settings.MAX_STUDENTS_PER_COURSE = 2
+
+    # Action
+    url = reverse('courses-list')
+    response = api_client.post(url, data={
+        'name': 'Python',
+        'students': [
+            {'name': 'Inna'},
+            {'name': 'Ivan'},
+            {'name': 'Sveta'}
+        ]
+    }
+                               )
+
+    # Assert
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+    assert Course.objects.count() == count
+
+
+@pytest.mark.django_db
+def test_update_course(api_client, course_factory):
+    courses = course_factory(_quantity=1)
+    course = courses[0]
+
+    url = reverse('courses-detail', args=[course.pk])
+    response = api_client.put(url, data={'name': 'Python', 'students': []})
+
+    assert response.status_code == status.HTTP_200_OK
+    obj = Course.objects.get(pk=course.pk)
+    assert obj.name == 'Python'
+    resp_json = response.json()
+    assert resp_json['name'] == 'Python'
+
+
+@pytest.mark.django_db
+def test_delete_course(api_client, course_factory):
+    courses = course_factory(_quantity=1)
+    course = courses[0]
+    count = Course.objects.count()
+
+    print(course.students)
+
+    url = reverse('courses-detail', args=[course.pk])
+    response = api_client.delete(url)
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert Course.objects.count() == count - 1
+    try:
+        obj = Course.objects.get(pk=course.pk)
+    except:
+        obj = None
+    assert obj is None
